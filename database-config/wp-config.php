70a139bd2b03ffab8f8b39c9c33e8b48be8bb9fc
tonydegreat1 <138535300+tonydegreat1@users.noreply.github.com>
2023-07-23 16:59:36 -0700

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.28.235.131' );
+define( 'DB_HOST', '34.66.174.138' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
